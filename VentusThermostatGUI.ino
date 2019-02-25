b4ca7fd680f229a1fb5b451a1fa456df3df33c76
Vu Q Le <vuql@uci.edu>
2019-02-25 00:31:04 -0800

    Updated GUI

--- VentusThermostatGUI.ino
+++ VentusThermostatGUI.ino
@@ -13,10 +13,13 @@
   MIT license, all text above must be included in any redistribution
  ****************************************************/
 
+
+
 #include <SPI.h>
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_HX8357.h>
 #include <Adafruit_STMPE610.h>
+#include "bitmaps.h"
 
 #define STMPE_CS 6
 #define TFT_CS   9
@@ -44,6 +47,9 @@
 #define GREEN2RED 4
 #define RED2GREEN 5
 
+#define CHAR_WIDTH 5
+#define CHAR_HEIGHT 8
+
 // gauge coordinates
 #define GAUGE_XPOS 0
 #define GAUGE_YPOS 0
@@ -60,9 +66,28 @@
 // up/down arrow coordinates
 #define ARROW_CENTER_X 400
 #define ARROW_CENTER_Y 160
-#define ARROW_WIDTH 100
+#define ARROW_WIDTH 120
 #define ARROW_HEIGHT 80
 #define ARROW_GAP 20
+
+// room title
+#define ROOM_ICON_XPOS 75
+#define ROOM_ICON_YPOS 260
+#define ROOM_ICON_SIZE 32
+#define ROOM_ICON_MARGIN 10
+#define ROOM_TITLE_FONTSIZE 3
+
+// fan button
+#define FAN_BUTTON_XPOS 335
+#define FAN_BUTTON_YPOS 4
+#define FAN_BUTTON_WIDTH 128
+#define FAN_BUTTON_HEIGHT 64
+
+// auto button
+#define AUTO_BUTTON_XPOS 335
+#define AUTO_BUTTON_YPOS 250
+#define AUTO_BUTTON_WIDTH 128
+#define AUTO_BUTTON_HEIGHT 64
 
 // calibration data for the raw touch data to the screen coordinates
 #define TS_MINX 150
@@ -86,51 +111,68 @@ Adafruit_STMPE610 ts = Adafruit_STMPE610(STMPE_CS);
 
 uint32_t runTime = -99999;       // time for next update
 
-int reading = 72; // Value to be displayed
+bool fanOn = false;
+bool autoOn = false;
 
 int tempF = 72;
 int lastTempF = 0;
 int humidityPercent = 50;
 int lastHumidityPercent = 0;
 
-void setup() {
 
+void setup() {
   tft.begin(HX8357D);
-  tft.setRotation(1);
+  tft.setRotation(1);  
   tft.fillScreen(BLACK);
-  
+  tft.drawBitmap(240 - 230/2, 160 - 170/2, loadingBitmap, 230, 170, WHITE);
+  delay(500);
+
   Serial.begin(115200);
-  while (!Serial) delay(10);
-  
+//  while (!Serial) delay(10);
+    
   if (!ts.begin()) {
     Serial.println("Couldn't start touchscreen controller");
     while (1);
   }
-  Serial.println("Touchscreen started");
   
+  Serial.println("TFT and TS started");
 
+  // clear to black before drawing
+  tft.fillScreen(BLACK);
 
   // draw up/down arrows
-  tft.fillTriangle(ARROW_CENTER_X-ARROW_WIDTH/2, ARROW_CENTER_Y-ARROW_GAP/2, ARROW_CENTER_X+ARROW_WIDTH/2, ARROW_CENTER_Y-ARROW_GAP/2, ARROW_CENTER_X, ARROW_CENTER_Y-ARROW_HEIGHT-ARROW_GAP/2, GREY); 
-  tft.fillTriangle(ARROW_CENTER_X-ARROW_WIDTH/2, ARROW_CENTER_Y+ARROW_GAP/2, ARROW_CENTER_X+ARROW_WIDTH/2, ARROW_CENTER_Y+ARROW_GAP/2, ARROW_CENTER_X, ARROW_CENTER_Y+ARROW_HEIGHT+ARROW_GAP/2, GREY);
+//  tft.fillTriangle(ARROW_CENTER_X-ARROW_WIDTH/2, ARROW_CENTER_Y-ARROW_GAP/2, ARROW_CENTER_X+ARROW_WIDTH/2, ARROW_CENTER_Y-ARROW_GAP/2, ARROW_CENTER_X, ARROW_CENTER_Y-ARROW_HEIGHT-ARROW_GAP/2, GREY); 
+//  tft.fillTriangle(ARROW_CENTER_X-ARROW_WIDTH/2, ARROW_CENTER_Y+ARROW_GAP/2, ARROW_CENTER_X+ARROW_WIDTH/2, ARROW_CENTER_Y+ARROW_GAP/2, ARROW_CENTER_X, ARROW_CENTER_Y+ARROW_HEIGHT+ARROW_GAP/2, GREY);
+
+  tft.drawBitmap(ARROW_CENTER_X-120/2, ARROW_CENTER_Y-180/2, arrowsBitmap, 120, 180, GREY);
 
   // draw the +/- on the arrows
   tft.setTextSize(8);
-  tft.setCursor(ARROW_CENTER_X-20, ARROW_CENTER_Y-60); 
   tft.setTextColor(WHITE);
-  
+  tft.setCursor(ARROW_CENTER_X-20, ARROW_CENTER_Y-70);
   tft.print("+");
-  tft.setCursor(ARROW_CENTER_X-20, ARROW_CENTER_Y+2);
-  tft.setTextColor(WHITE);
+  tft.setCursor(ARROW_CENTER_X-20, ARROW_CENTER_Y+10);
   tft.print("-");
+
+  tft.drawBitmap(ROOM_ICON_XPOS, ROOM_ICON_YPOS, homeIcon, 32, 32, WHITE);
+  tft.setCursor(ROOM_ICON_XPOS + ROOM_ICON_SIZE + ROOM_ICON_MARGIN, ROOM_ICON_YPOS+4);
+  tft.setTextSize(ROOM_TITLE_FONTSIZE);
+  tft.print("Kitchen");
+
+  tft.drawBitmap(valueXpos + CHAR_WIDTH*GAUGE_FONTSIZE*2 + 20, valueYpos, thermoIconF, 35, 56, WHITE);
+  tft.drawBitmap(valueXpos + CHAR_WIDTH*GAUGE_FONTSIZE*2 + 16, valueYpos+60, humidityIcon, 35, 56, WHITE);
+
+  
+  tft.drawBitmap(FAN_BUTTON_XPOS, FAN_BUTTON_YPOS, fanButton, FAN_BUTTON_WIDTH, FAN_BUTTON_HEIGHT, GREY);
+  tft.drawBitmap(AUTO_BUTTON_XPOS, AUTO_BUTTON_YPOS, autoButton, AUTO_BUTTON_WIDTH, AUTO_BUTTON_HEIGHT, GREY);
 
 }
 
 void loop() {
+  clearTouchBuffer();
+  
   if (ts.touched()) {
-    // retrieve a point
     TS_Point p = ts.getPoint();
-
     // get the latest point
     while (!ts.bufferEmpty()) {
         p = ts.getPoint();
@@ -138,7 +180,9 @@ void loop() {
     
     int py = map(p.x, TS_MINX, TS_MAXX, tft.height(), 0);
     int px = map(p.y, TS_MINY, TS_MAXY, 0, tft.width());
+    
 //    Serial.print("X = "); Serial.print(px); Serial.print("\tY = "); Serial.print(py);  Serial.print("\tPressure = "); Serial.println(p.z); 
+
     if ((px > ARROW_CENTER_X - ARROW_WIDTH/2) && (px < ARROW_CENTER_X + ARROW_WIDTH/2)) {
       if ((py > ARROW_CENTER_Y - ARROW_GAP/2 - ARROW_HEIGHT) && (py < ARROW_CENTER_Y - ARROW_GAP/2)) {
         tempF = tempF < 99 ? ++tempF : 99;
@@ -147,6 +191,21 @@ void loop() {
         tempF = tempF >= 0 ? --tempF : 0;
       }
     }
+    if ((px > FAN_BUTTON_XPOS) && (px < FAN_BUTTON_XPOS+FAN_BUTTON_WIDTH)) {
+      if ((py > FAN_BUTTON_YPOS) && (py < FAN_BUTTON_YPOS+FAN_BUTTON_HEIGHT)) {
+        toggleFan();
+        clearTouchBuffer();
+      }
+    }
+    
+    if ((px > AUTO_BUTTON_XPOS) && (px < AUTO_BUTTON_XPOS+AUTO_BUTTON_WIDTH)) {
+      if ((py > AUTO_BUTTON_YPOS) && (py < AUTO_BUTTON_YPOS+AUTO_BUTTON_HEIGHT)) {
+        toggleAuto();
+        clearTouchBuffer();
+      }
+    }
+
+    
     delay(100);
   }
 
@@ -157,14 +216,14 @@ void loop() {
 
     // draw or update the temp gauge when there is a change
     if(tempF != lastTempF) {
-      ringMeter(tempF, 43, 99, GAUGE_XPOS, GAUGE_YPOS, GAUGE_RAD, "F", BLUE2RED, valueXpos, valueYpos);
+      ringMeter(tempF, 43, 99, GAUGE_XPOS, GAUGE_YPOS, GAUGE_RAD, "", BLUE2RED, valueXpos, valueYpos, 20);
       lastTempF = tempF;
     }
     
 
     // draw or update the humidity gauge when there is a change
     if (humidityPercent != lastHumidityPercent) {
-      ringMeter(humidityPercent, 0, 99, GAUGE_RAD - HUM_GAUGE_RAD, GAUGE_RAD - HUM_GAUGE_RAD, HUM_GAUGE_RAD, "%", BLUE2RED, valueXpos, valueYpos+60);
+      ringMeter(humidityPercent, 0, 99, GAUGE_RAD - HUM_GAUGE_RAD, GAUGE_RAD - HUM_GAUGE_RAD, HUM_GAUGE_RAD, "", BLUE2RED, valueXpos, valueYpos+60, 20);
       lastHumidityPercent = humidityPercent;
     }
     
@@ -172,17 +231,48 @@ void loop() {
   }
 }
 
+void clearTouchBuffer() {
+  TS_Point p = ts.getPoint();
+
+  while (!ts.bufferEmpty()) {
+    p = ts.getPoint();
+  }
+}
+
+// toggle fan on/off
+void toggleFan() {
+  fanOn = fanOn ? false : true;
+  if (fanOn) {
+    tft.drawBitmap(FAN_BUTTON_XPOS, FAN_BUTTON_YPOS, fanButton, FAN_BUTTON_WIDTH, FAN_BUTTON_HEIGHT, WHITE);
+  }
+  else {
+    tft.drawBitmap(FAN_BUTTON_XPOS, FAN_BUTTON_YPOS, fanButton, FAN_BUTTON_WIDTH, FAN_BUTTON_HEIGHT, GREY);
+  }
+}
+
+
+// toggle auto on/off
+void toggleAuto() {
+  autoOn = autoOn ? false : true;
+  if (autoOn) {
+    tft.drawBitmap(AUTO_BUTTON_XPOS, AUTO_BUTTON_YPOS, autoButton, AUTO_BUTTON_WIDTH, AUTO_BUTTON_HEIGHT, WHITE);
+  }
+  else {
+    tft.drawBitmap(AUTO_BUTTON_XPOS, AUTO_BUTTON_YPOS, autoButton, AUTO_BUTTON_WIDTH, AUTO_BUTTON_HEIGHT, GREY);
+  }
+}
+
 //  draw the meter on the screen, returns x coord of righthand side
-int ringMeter(int value, int vmin, int vmax, int x, int y, int r, char *unit, byte scheme, int value_x, int value_y)
+int ringMeter(int value, int vmin, int vmax, int x, int y, int r, char *unit, byte scheme, int value_x, int value_y, int w)
 {
   // Minimum value of r is about 52 before value text intrudes on ring
   // drawing the text first is an option
   
   x += r; y += r;   // Calculate coords of centre of ring
 
-  int w = r / 8;    // Width of outer ring is 1/4 of radius
+//  int w = r / 8;    // Width of outer ring is 1/4 of radius
   
-  int angle = 150;  // Half the sweep angle of meter (300 degrees)
+  int angle = 130;  // Half the sweep angle of meter (300 degrees)
 
   int text_colour = 0; // To hold the text colour
 
@@ -234,9 +324,6 @@ int ringMeter(int value, int vmin, int vmax, int x, int y, int r, char *unit, by
     }
   }
 
-//  if (value < 10) {
-//    tft.fillRect(valueXpos, valueYpos, 105, 56, BLACK);
-//  }
   
   tft.setTextColor(WHITE, BLACK);
   tft.setCursor(value_x, value_y);
@@ -248,11 +335,6 @@ int ringMeter(int value, int vmin, int vmax, int x, int y, int r, char *unit, by
   
   tft.print(value);
   tft.print(unit);
-
-//  tft.setCursor(unitXpos, unitYpos);
-//  tft.setTextColor(WHITE, BLACK);
-//  tft.setTextSize(GAUGE_FONTSIZE);
-//  tft.print(units); 
 
   return x + r;
 }
